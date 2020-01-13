2fefad2ab776b1ec109369e3281b3c2753b6a9bb
David Botero Salazar <analiticaweb@pragma.com.co>
2020-01-13 15:12:35 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+var createsend = require('createsend-node');
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+const port = process.env.PORT || 3000;
+
+var auth = { apiKey: 'Lum/xCtzlsE/TskMv12rBLRN78Lo998VUwEl0nZsKvUv+lF2deMMU7xKeur0XYlMiGElj4KGaw3z1/O8jndJ+Ol3mA9g9+//Adzc5zQJJAwzWSy4lGmqCpfF17zZ5dTKKYJo8C2OEyfqEbZG1Bfqeg==' };
+var api = new createsend(auth);
+var listId = '418a0ee16a6d70e132cd99f7f509db74' // The ID of the list
+
+app.get("/", (req, res) => {
+    var user = {
+        EmailAddress: req.query.email,
+        CustomFields: [
+            { Key: req.query.campaign, Value: '1' }
+        ]
+    };
+    api.subscribers.updateSubscriber(listId, user.EmailAddress, user, (err, response) => {
+        if (err) {
+            res.send('err')
+        } else {
+            console.log(JSON.stringify(req.query));
+
+            res.redirect(req.query.destiny)
+        }
+    });
+})
+
+
+app.listen(port, () => {
+    console.log("Servidor inicilizado en ", port)
+});
