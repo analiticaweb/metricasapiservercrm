17941c7a9552dc3c952a58107eecdf4fcea01f40
David Botero Salazar <analiticaweb@pragma.com.co>
2020-01-22 09:57:07 -0500

    cambiamos lista y api key

--- index.js
+++ index.js
@@ -8,23 +8,29 @@ app.use(bodyParser.json());
 
 const port = process.env.PORT || 3000;
 
-var auth = { apiKey: 'Lum/xCtzlsE/TskMv12rBLRN78Lo998VUwEl0nZsKvUv+lF2deMMU7xKeur0XYlMiGElj4KGaw3z1/O8jndJ+Ol3mA9g9+//Adzc5zQJJAwzWSy4lGmqCpfF17zZ5dTKKYJo8C2OEyfqEbZG1Bfqeg==' };
+// var auth = { apiKey: 'Lum/xCtzlsE/TskMv12rBLRN78Lo998VUwEl0nZsKvUv+lF2deMMU7xKeur0XYlMiGElj4KGaw3z1/O8jndJ+Ol3mA9g9+//Adzc5zQJJAwzWSy4lGmqCpfF17zZ5dTKKYJo8C2OEyfqEbZG1Bfqeg==' };
+// var api = new createsend(auth);
+// var listId = '418a0ee16a6d70e132cd99f7f509db74' // The ID of the list
+
+
+var auth = { apiKey: 'idoUWtsrU/zgeegoO4GA5BGqfK+EViSYpKVqfUkI8hv1/H//0qDUZOjEQGwQUna2Qjfvxaan9rqDltN6JHLZ2UMVNJliw5vjfK70nJp+COTv92uVB9v6qDYJ5/uSi0lm6IWGtk/wdz/0AWqE+lvN5A==' };
 var api = new createsend(auth);
-var listId = '418a0ee16a6d70e132cd99f7f509db74' // The ID of the list
+var listId = '7f101d5c8761891d851e826c52b27e06' // The ID of the list
 
 app.get("/", (req, res) => {
     // let query = JSON.parse(Buffer.from(req.query.query, 'base64').toString());
     var user = {
-        EmailAddress: query.email,
+        EmailAddress: req.query.email,
         CustomFields: [
-            { Key: query.campaign, Value: '1' }
+            { Key: req.query.campaign, Value: '1' }
         ]
     };
+    // console.log({req})
     api.subscribers.updateSubscriber(listId, user.EmailAddress, user, (err, response) => {
         if (err) {
             res.send('err')
         } else {
-            res.redirect(query.destiny)
+            res.redirect(req.query.destiny)
         }
     });
 })
